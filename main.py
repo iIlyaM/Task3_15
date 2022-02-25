87be08771d3921c4d9caecf0c95ce54c942781a5
iIlyaM <imoldavskiy@yandex.ru>
2022-02-26 00:53:47 +0300

    pre-fin-commit : trouble with round, fix check

--- main.py
+++ main.py
@@ -22,44 +22,62 @@ def nums_list_to_triangle_list(nums: list) -> list[Triangle]:
 
 
 def are_triangle_similar(first_triangle: Triangle, second_triangle: Triangle):
-    for key_1 in first_triangle.get_angle_with_sides().keys():
-        for key_2 in second_triangle.get_angle_with_sides().keys():
-            if key_1 == key_2:
-                vert = list(zip(first_triangle.get_angle_with_sides()[key_1],
-                                second_triangle.get_angle_with_sides()[key_2]))
-                if vert[0][0] / vert[0][1] == vert[1][0] / vert[1][1]:
-                    return True
+    sides1 = first_triangle.get_sides_sizes()
+    sides1.sort()
+    sides2 = second_triangle.get_sides_sizes()
+    sides2.sort()
+
+    if round((sides1[0] / sides2[0]), 10) == round((sides1[1] / sides2[1]), 10) \
+            == round((sides1[2] / sides2[2]), 10):
+        return True
     return False
 
 
 def find_similar_figure_groups(triangles: list[Triangle]) -> list:
-    #todo не по индексам, а по элементам сразу
-
     groups = list()
     temp_group = list()
+
+    grouped = set()
+
     for i in range(len(triangles)):
+        triangle1 = triangles[i]
+
+        if triangle1 in grouped:
+            continue
+
         for j in range(len(triangles)):
+            triangle2 = triangles[j]
+
             if i == j:
                 continue
-            triangle1 = triangles[i]
-            triangle2 = triangles[j]
+
             if are_triangle_similar(triangle1, triangle2):
                 temp_group.append(triangle2)
+                grouped.add(triangle2)
         temp_group.append(triangle1)
+        grouped.add(triangle1)
         groups.append(temp_group)
         temp_group = list()
     return groups
 
 
+def read_answer(string: str):
+    if string == 'y' or string == 'Y':
+        return False
+    elif string == 'n' or string == 'N':
+        return True
+
+
 def main():
-    filename = input("Enter name of file: ")
-    data = nums_list_to_triangle_list(read_file(filename))
-    output_data = find_similar_figure_groups(data)
-    output_filename = input("Choose file for output: ")
-    write_file(output_filename, output_data)
-    print("Terminate the program?\n Print y - yes or n - no")
-
-
+    flag = True
+    while flag:
+        filename = input("Enter name of file: ")
+        data = nums_list_to_triangle_list(read_file(filename))
+        output_data = find_similar_figure_groups(data)
+        output_filename = input("Choose file for output: ")
+        write_file(output_filename, output_data)
+        print("Terminate the program?\n Print y - yes or n - no")
+        flag = read_answer(input())
 
 
 if __name__ == '__main__':
--- triangle.py
+++ triangle.py
@@ -1,7 +1,6 @@
 from math import *
 
 import numpy as np
-from linalg import det
 
 
 class Point:
@@ -21,8 +20,9 @@ class Segment:
         self.end_point = end_point
 
     def find_segment_length(self):
-        return sqrt((self.end_point.x - self.start_point.x) * (self.end_point.x - self.start_point.x) +
-                    (self.end_point.y - self.start_point.y) * (self.end_point.y - self.start_point.y))
+        return ((self.end_point.x - self.start_point.x) * (self.end_point.x - self.start_point.x) +
+                              (self.end_point.y - self.start_point.y) *
+                              (self.end_point.y - self.start_point.y)) ** 0.5
 
 
 class Triangle:
@@ -44,11 +44,6 @@ class Triangle:
                 or self.side_ab + self.side_ca < self.side_bc:
             raise Exception("A triangle with such vertices cannot exist")
 
-        # def is_point_on_line(seg: Segment, point: Point) -> bool:
-        #     if (point.y - seg.start_point.y) / (seg.end_point.y - seg.start_point.y) \
-        #             == (point.x - seg.start_point.x / (seg.end_point.x - seg.start_point.x)):
-        #         return True
-
         def calc_determinant():
             a = [[point1.x, point1.y, 1], [point2.x, point2.y, 1], [point3.x, point3.y, 1]]
             b = int(np.linalg.det(a))
@@ -57,25 +52,20 @@ class Triangle:
         def is_impossible_create():
             if calc_determinant() == 0:
                 return True
-            # if (point1.x == point2.x == point3.x) or (point1.y == point2.y == point3.y):
-            #     return True
-            # if is_point_on_line(self.segment_a, self.point3) and is_point_on_line(self.segment_b, self.point1) \
-            #         and is_point_on_line(self.segment_c, self.point3):
-            #     return True
             return False
 
         if is_impossible_create():
             raise Exception("Vertices lie on the same line")
 
-    def get_angle_with_sides(self):
-        angle_a = (acos((self.side_ab ** 2 + self.side_bc ** 2 - self.side_ca ** 2)
-                        / (2 * self.side_ab * self.side_bc)))
-        angle_b = (acos((self.side_bc ** 2 + self.side_ca ** 2 - self.side_ab ** 2)
-                        / (2 * self.side_bc * self.side_ca)))
-        angle_c = (acos((self.side_ca ** 2 + self.side_ab ** 2 - self.side_bc ** 2)
-                        / (2 * self.side_ca * self.side_ab)))
-        return {angle_a: [self.side_ab, self.side_bc], angle_b: [self.side_bc, self.side_ca],
-                angle_c: [self.side_ca, self.side_ab]}
+    # def get_angle_with_sides(self):
+    #     angle_a = (acos((self.side_ab ** 2 + self.side_bc ** 2 - self.side_ca ** 2)
+    #                     / (2 * self.side_ab * self.side_bc)))
+    #     angle_b = (acos((self.side_bc ** 2 + self.side_ca ** 2 - self.side_ab ** 2)
+    #                     / (2 * self.side_bc * self.side_ca)))
+    #     angle_c = (acos((self.side_ca ** 2 + self.side_ab ** 2 - self.side_bc ** 2)
+    #                     / (2 * self.side_ca * self.side_ab)))
+    #     return {round(angle_a, 5): [self.side_ab, self.side_bc], round(angle_b, 5): [self.side_bc, self.side_ca],
+    #             round(angle_c, 5): [self.side_ca, self.side_ab]}
 
     def get_sides_sizes(self) -> list:
         return [self.side_ab, self.side_bc, self.side_ca]
